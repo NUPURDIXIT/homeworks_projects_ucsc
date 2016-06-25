ff3721a043bd48c5597be39aa05e57da42a02b8b
NUPUR DIXIT <NUPURDIXIT13@gmail.com>
2016-06-25 15:48:34 -0700

    Validation for input fields as negative

--- apache/JSFinal/LoanCalculator.js
+++ apache/JSFinal/LoanCalculator.js
@@ -23,19 +23,23 @@ function calculate() {
 
     
     var monthlyPayment = loanAmount * AnnIntPerMonth / (1 - (Math.pow(1 / (1 + AnnIntPerMonth), rePayYearsInMonths)));
-    monthlyPayment = monthlyPayment.toFixed(2);
-    output1 = '$' + monthlyPayment
-    document.getElementById('output1').innerHTML = output1;
 
-    var totalPayment = monthlyPayment*rePayYearsInMonths;
-    totalPayment = totalPayment.toFixed(2);
-    output2 = '$' + totalPayment
-    document.getElementById('output2').innerHTML = output2;
 
-    var totalInterest = (rePayYearsInMonths * monthlyPayment) - loanAmount;
-    totalInterest = totalInterest.toFixed(2);
-    output3 = '$' + totalInterest
-    document.getElementById('output3').innerHTML = output3;
+    if(isFinite(monthlyPayment)){
+        monthlyPayment = monthlyPayment.toFixed(2);
+        output1 = '$' + monthlyPayment
+        document.getElementById('output1').innerHTML = output1;
+
+        var totalPayment = monthlyPayment*rePayYearsInMonths;
+        totalPayment = totalPayment.toFixed(2);
+        output2 = '$' + totalPayment
+        document.getElementById('output2').innerHTML = output2;
+
+        var totalInterest = (rePayYearsInMonths * monthlyPayment) - loanAmount;
+        totalInterest = totalInterest.toFixed(2);
+        output3 = '$' + totalInterest
+        document.getElementById('output3').innerHTML = output3;
+    }
 
     console.log("zipcode is"+zipcode)
     if(zipcode!=''){
@@ -48,18 +52,18 @@ function calculate() {
 
 function isValidInput(loanAmount, AnnualInterest, rePayYears){
     var valid = true;
-    if(loanAmount==''){
+    if(loanAmount=='' || loanAmount<0){
         document.getElementById("errors1").innerHTML="Please enter a valid loan amount";
         valid = false;
         
     }
     
-    if(AnnualInterest==''){
+    if(AnnualInterest=='' || AnnualInterest<0){
         document.getElementById("errors2").innerHTML="Please enter a valid Annual Interest";
         valid = false;
     }
 
-    if(rePayYears==''){
+    if(rePayYears=='' || rePayYears<0){
         document.getElementById("errors3").innerHTML="Please enter valid Years";
         valid = false;
     }
