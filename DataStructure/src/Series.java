f8d0803d741ef34c2c773f8a41c97c3a710298d6
Arnav Awasthi <arnavawasthi@gmail.com>
2016-07-09 11:35:53 -0700

    chess needs to be fixed

--- DataStructure/src/Series.java
+++ DataStructure/src/Series.java
@@ -4,14 +4,17 @@ import java.math.BigInteger;
 public class Series {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//int sum;
 		
-		//seriesConvergeAbsolutely();
-		//System.out.println("Sum of Geometric series is : "+bg1.longValue());
+		seriesConvergeAbsolutely();
+		
 		harmonicSeries();
-		numberOfPrimes();
 		
+		printForPrimes();
+		for(int i=100;i<=1000000;i*=10){
+			numberOfPrimes(i);
+		}
+		
+		chess();
 
 	}
 	
@@ -20,42 +23,65 @@ public class Series {
 		
 	}
 	
-	/*public static void seriesConvergeAbsolutely(){
-		//BigDecimal geometricSum=new BigDecimal(0);
-		//double geometricSum = 0;
-		BigInteger bg1=new BigDecimal(geometricSum).toBigInteger();
+	public static void seriesConvergeAbsolutely(){
+		
+		double geometricSum = 0;
+		
 		for(int i=0;i<=100;i++){
-			geometricSum+=Math.pow(1/2, i);
+			
+			geometricSum+=Math.pow(0.5, i);
 		}
-		BigInteger bg1=new BigDecimal(geometricSum).toBigInteger();
-		System.out.println("Sum of Geometric series is : "+bg1);
-		//return geometricSum;
-	}*/
+		
+		System.out.println("Sum of Geometric series is : "+(int)geometricSum);
+		
+	}
 	
 	
 	public static void harmonicSeries(){
-		long harmonicSum=0;
+		double harmonicSum=0;
 		for(int i=1;i<=100;i++){
-			harmonicSum+=(1/i);
+			
+			harmonicSum+=(1.0/i);
 		}
-		double sum=Math.log(100)+0.57721566;
+		double sumByFormula=Math.log(100)+0.57721566;
 		
-		System.out.println("Sum of harmonic series is: "+harmonicSum);
-		System.out.println("Sum of right hand side of harmonic series is: "+sum);
+		System.out.println("Sum of given harmonic series is: "+harmonicSum);
+		System.out.println("Sum of given harmonic series computed through formula is: "+sumByFormula);
 	}
 	
 	
-	public void chess(){
+	public static void chess(){
+		double sumOfRice=0;
+		double power=0;
+		long sumOfRiceByFormula=0L;
+		for(int i=1;i<=64;i++){
+			power=Math.pow(2,i-1);
+			sumOfRice+=power;
+			
+			System.out.println("On square "+i+" = "+ (long)power);
+		}
+		sumOfRiceByFormula+=Math.pow(2,64)-1;
+		System.out.println("Total rice = "+(long)sumOfRice);
+		System.out.println("Total rice computed by the formula for geometric series = "+sumOfRiceByFormula);
 		
 	}
 	
-	public static void numberOfPrimes(){
+	public static void printForPrimes(){
+		System.out.println("n         #P(n)    n/log n -1");
+		//System.out.println();
+		System.out.println("-------------------------------------------");
+	}
+	
+	public static void numberOfPrimes(int num){
 		int count=0;
 		boolean flag=true;
-		for(int num=4;num<100000000;num++){
+		double valueByFormula=0;
+		
+		for(int i=4;i<=num;i++){
 			flag=true;
-			for(int i=2;i<=Math.sqrt(num);i++){
-				if(num%i==0){
+			int sqrt=(int)Math.sqrt(i);
+			for(int j=2;j<=sqrt;j++){
+				if(i%j==0){
 					flag=false;
 					break;
 				}
@@ -64,8 +90,13 @@ public class Series {
 			if(flag){
 				count++;
 			}
+			valueByFormula=((num*1.0)/(Math.log(num)-1));
+			
 		}
-		System.out.println("Number of primes till 100 are :"+(count+2));
+		
+		
+		System.out.format("%-10d %-8d %-8d",num,count+2,(int)valueByFormula);
+		System.out.println();
 	}
 	
 	
