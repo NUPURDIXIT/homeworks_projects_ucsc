b82c2adaf927ed9835a7ca11705dc1ce4ab1e40c
NUPUR DIXIT <NUPURDIXIT13@gmail.com>
2016-08-14 23:55:27 -0700

    Quicksort via Linkedlist in working condition

--- DataStructure/src/homeworkDay5/SlistQuickSort.java
+++ DataStructure/src/homeworkDay5/SlistQuickSort.java
@@ -1,96 +1,112 @@
 package HomeworkDay5;
 
 /**
- * File Name: SlistQuickSort 
- * Sort int slist using Quick Sort
+ * File Name: SlistQuickSort Sort int slist using Quick Sort
  * 
  * @author Jagadeesh Vasudevamurthy
  * @year 2016
  */
 
 /*
- * To compile you require: IntUtil.java RandomInt.java IntSlist2.java SlistSort.java SlistQuickSort.java
+ * To compile you require: IntUtil.java RandomInt.java IntSlist2.java
+ * SlistSort.java SlistQuickSort.java
  */
 
-class SlistQuickSort extends SlistSort{
- 
-  @Override
-  protected void sort(boolean ascend) { 
-    //WRITE CODE HERE
-	  node n=a.first;
-	  if(n==null){System.out.println("n is null");}
-	 // System.out.println("n is "+n.d);
-	  a.first=quickSort(n);
-  }
-  
-  private node quickSort(node n){
-	  System.out.println("QuickSort starts");
-	  //n=first;
-	  //System.out.println(" n is "+n.d);
-	  if (n==null ||n.next==null){
-		  return n;
-	  }
-	  return quick(n,null);
-  }
-  private node quick(node start,node end){
-	  System.out.println("Enters quick");
-	  if(start==null || start==end || start.next==end){
-		  System.out.println("Enters if condition");
-		  return start;
-	  }
-	  //System.out.println("Start is "+start.d);
-	  node[] result=partition(start,end);
-	  node resultLeft=quick(result[0],result[1]);
-	  node resultRight=quick(result[1].next,end);
-	  
-	  return resultLeft;
-	   
-  }
-  
-  private node[] partition(node start,node end){
-	  if(start==null || start==end || start.next==end){
-		  return new node[]{start,start};
-	  }
-	  
-	  node dummy=new node(0,0);
-	  dummy.next=start;
-	  for(node j=start; j!=null && j.next!=null && j.next!=end ;j=j.next ){
-		  while(j.next!=null && j.next.d<=start.d){
-			  node temp=j.next;
-			  j.next=j.next.next;
-			  temp.next=dummy.next;
-			  dummy.next=temp;
-		  }
-	  }
-	  return new node[]{dummy.next,start};
-  }
-   
-  private void sort1(IntSlist2 a) {
-	   System.out.println("---------SORT1 Start------------"); 
-	    this.a = a ;
-//	   
-//	    display = false ;
-//	    int os = a.size() ;
-//	    System.out.println(":size if a is:"+os);
-//	    if (os > 0 && os < 20) {
-//	      display = true ;
-//	    }
-	    sort(true); //THIS CODE IS WRITTEN BY USER
-	    
-	   // if (display) {
-	      a.pLn() ;
-	   // }
-	   // u.printStatistics(a.size(),numCompare,numSwap,0);
-	    System.out.println("---------SORT1 End---------------");
-	  }
-  public static void main(String[] args) {
-    System.out.println("SlistQuickSort.java");
-    int a[]={3,4,2,1,5};
-    IntSlist2 s=IntSlist2.buildSlist(a);
-    s.pLn();
-    SlistQuickSort u = new SlistQuickSort() ;
-    u.sort1(s);
-    //u.testBench();
-  }
+class SlistQuickSort extends SlistSort {
 
+	@Override
+	protected void sort(boolean ascend) {
+		// WRITE CODE HERE
+		// print(a.first);
+		node root = a.first;
+		node temp = quickSort(root, root, null);
+
+		this.a.first = temp;
+
+		// print(temp);
+	}
+
+	// public static Node solution(Node root){
+	// return quickSort(root, root, null);
+	// }
+
+	private void print(node n) {
+		while (n != null) {
+			System.out.print(n.d + ":" + n.t + " ");
+			n = n.next;
+		}
+		System.out.println();
+	}
+
+	public node quickSort(node root, node start, node end) {
+		if (start == null || start == end)
+			return start;
+
+		node[] nodes = partition(start, end);
+		// if(nodes[0] != null) nodes[0].print("Before Pivot: ");
+		// if(nodes[1] != null) nodes[1].print("From Pivot: ");
+		numRecursion++;
+		node start1 = quickSort(root, nodes[0], nodes[1]);
+		// start1.print();
+		numRecursion++;
+		node start2 = quickSort(root, nodes[1].next, end);
+		nodes[1].next = start2;
+		// print(start1);
+		return start1;
+	}
+
+	public node[] partition(node start, node end) {
+
+		node lowRoot = null, lowList = null, highRoot = null, highList = null;
+		node pivot = start;
+
+		int x = pivot.d;
+		start = start.next;
+		while (start != null && start != end) {
+			numCompare++;
+			if (start.d < x) {
+				if (lowRoot == null) {
+					lowRoot = start;
+				} else {
+					lowList.next = start;
+					numSwap++;
+				}
+				lowList = start;
+			} else {
+				if (highRoot == null) {
+					highRoot = start;
+				} else {
+					highList.next = start;
+					numSwap++;
+				}
+				highList = start;
+			}
+			start = start.next;
+		}
+
+		pivot.next = highRoot;
+
+		// Setting last node's next to null (terminating the list)
+		node[] nodes = new node[] { pivot, pivot };
+		if (lowList != null) {
+			lowList.next = null;
+			lowList.next = pivot;
+			nodes = new node[] { lowRoot, pivot };
+
+		}
+		if (highList != null) {
+			highList.next = end;
+
+		} else {
+			pivot.next = end;
+		}
+
+		return nodes;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("SlistQuickSort.java");
+		SlistQuickSort u = new SlistQuickSort();
+		u.testBench();
+	}
 }
